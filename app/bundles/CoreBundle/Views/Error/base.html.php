3e88d88e70786c878987912fde07dcf5b1332c97
samuelfaith <sam@xperx.ai>
2021-03-31 15:40:34 +0700

    Update base.html.php

--- app/bundles/CoreBundle/Views/Error/base.html.php
+++ app/bundles/CoreBundle/Views/Error/base.html.php
@@ -43,7 +43,7 @@ $message = $view['slots']->get('message', 'mautic.core.error.generic');
                 <footer class="text-right">Mautibot</footer>
             </blockquote>
             <div class="pull-right">
-                <a class="text-muted" href="http://mau.tc/report-issue" target="_new"><?php echo $view['translator']->trans('mautic.core.report_issue'); ?></a>
+                <a class="text-muted" href="https://www.xperx.ai/" target="_new"><?php echo $view['translator']->trans('mautic.core.report_issue'); ?></a>
             </div>
         </div>
         <?php endif; ?>
